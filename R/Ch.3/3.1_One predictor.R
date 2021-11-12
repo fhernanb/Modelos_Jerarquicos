1ae5cacdb97fb97a031637a594e0b4ac09ef3f65
fhernanb <fhernanb@unal.edu.co>
2021-11-12 14:30:49 -0500

    Se modificó este ejemplo :bomb:

--- R/Ch.3/3.1_One predictor.R
+++ R/Ch.3/3.1_One predictor.R
@@ -1,36 +1,24 @@
-## Read the data
-# Data are at http://www.stat.columbia.edu/~gelman/arm/examples/child.iq
+# ------------------------------------------------------------------------------
+# Archivo modificado a partir del original -------------------------------------
+# ------------------------------------------------------------------------------
 
 library("arm")
 library(haven)
 
 kidiq <- read_dta("Datos/child.iq/kidiq.dta")
-attach(kidiq)
+
+# attach(kidiq) # Oculte el attach porque NO ME GUSTA
 
 ## Plot Figure 3.1
-kidscore.jitter <- jitter(kid_score)
+library(ggplot2)
 
-jitter.binary <- function(a, jitt=.05){
-   ifelse (a==0, runif (length(a), 0, jitt), runif (length(a), 1-jitt, 1))
-}
-
-jitter.mom_hs <- jitter.binary(mom_hs)
-
-fit.0 <- lm (kid_score ~ mom_hs)
-display(fit.0)
-
-plot(jitter.mom_hs,kidscore.jitter, xlab="Mother completed high school", 
-  ylab="Child test score",pch=20, xaxt="n", yaxt="n")
-axis (1, seq(0,1))
-axis (2, c(20,60,100,140))
-abline (fit.0)
+ggplot(kidiq, aes(x=mom_hs, y=kid_score)) +
+  geom_jitter(width = 0.02)
 
 ## Plot Figure 3.2
-fit.1 <- lm (kid_score ~ mom_iq)
+ggplot(kidiq, aes(x=mom_iq, y=kid_score)) + 
+  geom_point()+
+  geom_smooth(method=lm, se=FALSE)
 
-plot(mom_iq,kid_score, xlab="Mother IQ score", 
-  ylab="Child test score",pch=20, xaxt="n", yaxt="n")
-axis (1, c(80,100,120,140))
-axis (2, c(20,60,100,140))
-abline (fit.1)
-
+fit1 <- lm (kid_score ~ mom_iq, data=kidiq)
+summary(fit1)
