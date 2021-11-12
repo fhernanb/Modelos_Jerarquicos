d0888ddbff80ce37399029998b5a3229be1c409c
fhernanb <fhernanb@unal.edu.co>
2021-11-12 14:48:10 -0500

    Otro ejemplo nuevo :smile:

---
+++ R/Ch.14/Chapter 14.R
@@ -0,0 +1,23 @@
+# ------------------------------------------------------------------------------
+# Archivo modificado a partir del original -------------------------------------
+# ------------------------------------------------------------------------------
+
+# Ejemplo sobre votaciones
+
+library (foreign)
+url <- 'Datos/death.polls/polls.dta'
+datos <- read.dta(file=url)
+
+# Usando la inf del survey = 9158
+datis <- subset(datos, survey == 9158)
+
+library(lme4)
+M1 <- glmer(bush ~ female + black + (1 | state), 
+            data=datis, family=binomial(link="logit"))
+summary(M1)
+
+# Other information
+coef(M1)
+ranef(M1)
+fixef(M1)
+
